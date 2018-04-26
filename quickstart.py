74f52ad935d1051eee7685c0d34adba5226bee74
Richie Zeng <richzeng@gmail.com>
2018-04-26 00:18:05 -0700

    Updated quickstart to do follow unfollow cycles

--- quickstart.py
+++ quickstart.py
@@ -5,9 +5,11 @@ from tempfile import gettempdir
 from selenium.common.exceptions import NoSuchElementException
 
 from instapy import InstaPy
+from instapy.util import format_number, update_activity
+from instapy.accounts import WINERYWORLD_USERNAME, WINERYWORLD_PW
 
-insta_username = ''
-insta_password = ''
+insta_username = WINERYWORLD_USERNAME
+insta_password = WINERYWORLD_PW
 
 # set headless_browser=True if you want to run InstaPy on a server
 
@@ -16,41 +18,73 @@ insta_password = ''
 #   Settings.database_location = '/path/to/instapy.db'
 #   Settings.chromedriver_location = '/path/to/chromedriver'
 
-session = InstaPy(username=insta_username,
-                  password=insta_password,
-                  headless_browser=False,
-                  multi_logs=True)
 
-try:
-    session.login()
+def follow_like(session):
+    hashtags = ['wine', 'winery', 'wineries', 'winelover', 'winemaker', 'winetime', 'wines', 'winenight', 'winetasting', 'wineporn', 'winestagram', 'winelife', 'winegeek', 'winetour', 'winewinewine', 'wineclub', 'winemaking', 'vineyard', 'winebar', 'winedown']
+    session.like_by_tags(hashtags, amount=1)
+
+
+def unfollow(session):
+    session.unfollow_users(amount=10, unfollow_after=72*60*60)
+
+
+def get_own_following_count(session):
+    session.browser.get('https://www.instagram.com/' + session.username.strip())
+    # update server calls
+    update_activity()
+
+    try:
+        allfollowing = format_number(
+            session.browser.find_element_by_xpath("//li[3]/a/span").text)
+        return allfollowing
+    except NoSuchElementException:
+        logger.warning('There are 0 people to follow')
+        return 0
+
+
+def run():
+    session = InstaPy(username=insta_username,
+                      password=insta_password,
+                      headless_browser=True,
+                      multi_logs=True)
 
     # settings
     session.set_relationship_bounds(enabled=True,
-				 potency_ratio=-1.21,
-				  delimit_by_numbers=True,
-				   max_followers=4590,
-				    max_following=5555,
-				     min_followers=45,
-				      min_following=77)
-    session.set_do_comment(True, percentage=10)
-    session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
-    session.set_dont_include(['friend1', 'friend2', 'friend3'])
+                 potency_ratio=None,
+                  delimit_by_numbers=True,
+                   max_followers=9999,
+                    max_following=1000,
+                     min_followers=0,
+                      min_following=0)
     session.set_dont_like(['pizza', 'girl'])
+    session.set_do_follow(enabled=True, percentage=50, times=1)
+    #session.set_dont_unfollow_active_users(enabled=True, posts=5)
 
-    # actions
-    session.like_by_tags(['natgeo'], amount=1)
 
-except Exception as exc:
-    # if changes to IG layout, upload the file to help us locate the change
-    if isinstance(exc, NoSuchElementException):
-        file_path = os.path.join(gettempdir(), '{}.html'.format(time.strftime('%Y%m%d-%H%M%S')))
-        with open(file_path, 'wb') as fp:
-            fp.write(session.browser.page_source.encode('utf8'))
-        print('{0}\nIf raising an issue, please also upload the file located at:\n{1}\n{0}'.format(
-            '*' * 70, file_path))
-    # full stacktrace when raising Github issue
-    raise
+    try:
+        session.login()
+        while True:
+            following_count = get_own_following_count(session)
+            print "Currently following " + str(following_count) + " users"
+            if following_count < 1200:
+                print "Starting follow/like cycle"
+                follow_like(session)
+            else:
+                print "Starting unfollow cycle"
+                unfollow(session)
+    except Exception as exc:
+        # if changes to IG layout, upload the file to help us locate the change
+        if isinstance(exc, NoSuchElementException):
+            file_path = os.path.join(gettempdir(), '{}.html'.format(time.strftime('%Y%m%d-%H%M%S')))
+            with open(file_path, 'wb') as fp:
+                fp.write(session.browser.page_source.encode('utf8'))
+            print('{0}\nIf raising an issue, please also upload the file located at:\n{1}\n{0}'.format(
+                '*' * 70, file_path))
+        # full stacktrace when raising Github issue
+        raise
+    finally:
+        session.end()
 
-finally:
-    # end the bot session
-    session.end()
+
+if __name__ == "__main__":
+    run()
